3b60147afc86d088d55d366b443fc7c43f2e5313
AndyMB94 <andy.mallcco@tecsup.edu.pe>
2024-11-25 00:09:09 -0500

    modificando admin

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Usuario, Cliente, Proveedor, Factura
 
-# Register your models here.
+# Registra los modelos para que aparezcan en el panel de administración
+admin.site.register(Usuario)
+admin.site.register(Cliente)
+admin.site.register(Proveedor)
+admin.site.register(Factura)
